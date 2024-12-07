c3aabb58632636399c081a32d2901ce63318264c
manoj-bhaskaran <manoj.bhaskaran@gmail.com>
2024-12-07 22:45:13 +0530

    Changed doub;e backslashes to single

--- model_runner.py
+++ model_runner.py
@@ -31,7 +31,7 @@ import os
 # Set up command-line argument parsing
 parser = argparse.ArgumentParser(description='Expense Predictor')
 parser.add_argument('--future_date', type=str, help='Future date for prediction (e.g., 31/12/2025)')
-parser.add_argument('--excel_dir', type=str, default=r'C:\\Users\\manoj\\Downloads', help='Directory where the Excel file is located')
+parser.add_argument('--excel_dir', type=str, default=r'C:\Users\manoj\Downloads', help='Directory where the Excel file is located')
 parser.add_argument('--excel_file', type=str, help='Name of the Excel file containing additional data')
 args = parser.parse_args()
 
@@ -57,7 +57,7 @@ elif not args.excel_file:
 TRANSACTION_AMOUNT_LABEL = 'Tran Amt'
 
 # Define file path for input data
-file_path = r'D:\\Python\\Projects\\Expense Predictor\\trandata.csv'  # Use raw string literal
+file_path = r'D:\Python\Projects\Expense Predictor\trandata.csv'  # Use raw string literal
 
 # Preprocess input data
 X_train, y_train, df = preprocess_and_append_csv(file_path, excel_path=excel_path)  # Optional Excel path
@@ -134,5 +134,5 @@ for model_name, model in models.items():
     predicted_df = pd.DataFrame({'Date': future_dates, f'Predicted {TRANSACTION_AMOUNT_LABEL}': y_predict})
 
     # Save predictions to a CSV file
-    output_path = rf'D:\\Python\\Projects\\Expense Predictor\\future_predictions_{model_name.replace(" ", "_").lower()}.csv'
+    output_path = rf'D:\Python\Projects\Expense Predictor\future_predictions_{model_name.replace(" ", "_").lower()}.csv'
     write_predictions(predicted_df, output_path)
