b2747dcda8f3f29df96e80c0bb71b4fa6120baed
manoj-bhaskaran <manoj.bhaskaran@gmail.com>
2025-11-16 04:24:01 +0530

    feat: Update label creation function to print all needed labels

--- create_github_issues.sh
+++ create_github_issues.sh
@@ -253,7 +253,7 @@ check_and_create_labels() {
     IFS=',' read -ra LABELS <<< "$needed_labels"
     local total_labels=${#LABELS[@]}
     local current=0
-    print_info "Here 1"
+    print_info "${LABELS[@]}"
     for label in "${LABELS[@]}"; do
         ((current++))
         # Trim whitespace
